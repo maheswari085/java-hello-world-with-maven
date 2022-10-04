1ee811ad910528c730bbb5039b44dae719da24d0
senthil085 <senthilnathan.it@gmail.com>
2022-10-03 19:37:07 -0700

    Adding content to check continuous build

--- src/main/java/hello/Greeter.java
+++ src/main/java/hello/Greeter.java
@@ -2,6 +2,7 @@ package hello;
 
 public class Greeter {
     public String sayHello() {
+        System.out.println("New line added");
         return "DevOps Project for AchiStar Technologies";
     }
 }
