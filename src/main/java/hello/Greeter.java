c190c2ea7d554b7651ebc95b3ffed6131cd8cc22
maheswari085 <109555883+maheswari085@users.noreply.github.com>
2022-10-05 16:16:36 -0700

    Update Greeter.java

--- src/main/java/hello/Greeter.java
+++ src/main/java/hello/Greeter.java
@@ -2,7 +2,7 @@ package hello;
 
 public class Greeter {
     public String sayHello() {
-        System.out.println("New line added");
+        System.out.println("New line added::");
         return "DevOps Project for AchiStar Technologies";
     }
 }
